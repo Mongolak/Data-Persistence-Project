c3e5dbbfaf1eddc1d5ed71719eeb7bcf0ed8fb51
Mongolak <creacionesweb@mongolk.pro>
2025-05-06 15:20:40 +0200

    Quit-button-added

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -12,20 +13,20 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -61,7 +62,7 @@ public class MainManager : MonoBehaviour
             }
 
             //Condicional para volver a cargar la escena del Menú principal.
-            if(Input.GetKeyDown(KeyCode.Escape))
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
 
                 SceneManager.LoadScene(0);
@@ -86,6 +87,22 @@ public class MainManager : MonoBehaviour
     {
 
         SceneManager.LoadScene(1);
+
+    }
+
+    public void Exit()
+    {
+
+#if UNITY_EDITOR
+
+        EditorApplication.ExitPlaymode();
+
+#else
+    
+    Applicattion.Quit();
+    
+    
+#endif
 
     }
 }
